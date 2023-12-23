4225bfb0dabf365d14fd05eeba6d284c2ebf5bee
Cdavison-development <cdog.blues@outlook.com>
2023-12-23 12:50:34 +0000

    removed old files

--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-double distance(double x1, double y1, double x2, double y2);
-double **generateDistanceMatrix(double **coords, int numOfCoords);
-
-#endif // UTILS_H
