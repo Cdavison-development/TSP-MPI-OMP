db459927e4a45eb026a186def6415e3dd4cae4d1
Cdavison-development <cdog.blues@outlook.com>
2023-12-23 12:51:39 +0000

    removed old files

--- coordReader.h
+++
@@ -1,7 +0,0 @@
-#ifndef COORDREADER_H
-#define COORDREADER_H
-
-int readNumOfCoords(char *filename);
-double **readCoords(char *filename, int numOfCoords);
-void writeTourToFile(int *tour, int tourLength, char *filename);
-#endif // COORDREADER_H
